619586c90f53e87e2462a3fa6418e947dcf72edc
Deborah Pickett <github@dp.futzle.com>
2011-12-27 05:40:50 +0000

    GPL notice on all files (except JSON). Shrinkify partition JSON to remove useless and seldom-used buttons.

--- L_CaddxNX584Security.lua
+++ L_CaddxNX584Security.lua
@@ -1,5 +1,20 @@
 --
--- Luup plugin for GE/Networx/Caddx NX-584/NX-8E security system interface.
+-- GE Caddx Network NX-584/NX-8E Alarm Plugin
+-- Copyright (C) 2009-2011 Deborah Pickett
+-- 
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --
 -- Version 0.2 2011-04-27 by Deborah Pickett
 -- * Panic modes (medical, fire, police)
